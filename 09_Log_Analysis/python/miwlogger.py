6848db1c143b4ae004eb6956e6ddd9ae64034610
yassine <132344690+mchyasn@users.noreply.github.com>
2025-06-18 11:52:41 +0000

    Remove 09_Log_Analysis folder

--- 09_Log_Analysis/python/miwlogger.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-formatter = logging.Formatter("[%(levelname)s@%(asctime)s][%(filename)s:%(lineno)s - %(funcName)2s() ] %(message)s")
-stdout_handler = logging.StreamHandler()
-stdout_handler.setFormatter(formatter)
-logger = logging.getLogger(__name__)
-loglevel = logging.DEBUG
-logger.setLevel(loglevel)
-logger.addHandler(stdout_handler)
